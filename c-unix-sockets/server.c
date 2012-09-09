c207b40dd2b4dae642933936d91e7b2fa20461ee
Radosław Szymczyszyn <lavrin@gmail.com>
2012-09-10 00:28:09 +0200

    c-unix-sockets: server on its way to libev

--- c-unix-sockets/server.c
+++ c-unix-sockets/server.c
@@ -16,40 +16,36 @@
 #define BUFSIZE 4096
 
 static int
-bind_and_listen(const char *path);
+bind_and_listen (const char *path);
+
+ev_io listen_watcher;
 
 static void
-handle_connection(int sfd);
+listen_cb (EV_P_ ev_io *w, int revents);
+
+static void
+client_cb (EV_P_ ev_io *w, int revents);
 
 int
-main(int argc, char *argv[])
+main (int argc, char *argv[])
 {
-    int sfd, cfd;
-    struct sockaddr_un peer_addr;
-    socklen_t peer_addr_size;
+    int sfd;
+    struct ev_loop *loop = EV_DEFAULT;
 
     if (access(MY_SOCK_PATH, F_OK) == 0)
         unlink(MY_SOCK_PATH);
 
     sfd = bind_and_listen(MY_SOCK_PATH);
 
+    /* set sfd to non-blocking mode before using with libev */
+
+    /* how to store sfd in the watcher? */
+    ev_io_init (&listen_watcher, listen_cb, sfd, EV_READ);
+    ev_io_start (loop, &listen_watcher);
+
     /* Now we can accept incoming connections one
        at a time using accept(2) */
 
-    for(;;) {
-        peer_addr_size = sizeof(struct sockaddr_un);
-        cfd = accept(sfd, (struct sockaddr *) &peer_addr,
-                     &peer_addr_size);
-        if (cfd == -1)
-            handle_error("accept");
-
-        /* Code to deal with incoming connection(s)... */
-        pid_t pid = fork();
-        if (pid == 0)
-            handle_connection(cfd);
-        else if (pid == -1)
-            handle_error("fork");
-    }
 
     /* When no longer required, the socket pathname, MY_SOCK_PATH
        should be deleted using unlink(2) or remove(3) */
@@ -78,6 +74,25 @@ bind_and_listen(const char *path)
         handle_error("listen");
 
     return sfd;
+}
+
+static void
+listen_cb (EV_P_ ev_io *w, int revents)
+{
+    for(;;) {
+        peer_addr_size = sizeof(struct sockaddr_un);
+        cfd = accept(sfd, (struct sockaddr *) &peer_addr,
+                     &peer_addr_size);
+        if (cfd == -1)
+            handle_error("accept");
+
+        /* Code to deal with incoming connection(s)... */
+        pid_t pid = fork();
+        if (pid == 0)
+            handle_connection(cfd);
+        else if (pid == -1)
+            handle_error("fork");
+    }
 }
 
 void
