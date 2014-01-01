e56da280229e817273da07bd9ebf0adc0b2202ad
Radek Szymczyszyn <lavrin@gmail.com>
2014-01-01 13:57:21 +0100

    Make a safe wrapper around regex.h regcomp/regexec
    Next: more error handling, standalone library

--- rust/grep4.rs
+++ rust/grep4.rs
@@ -1,6 +1,5 @@
-use std::libc::{c_char, c_int, size_t};
+use std::libc::{c_char, c_int, c_void, size_t, malloc, free};
 use std::ptr;
-use std::vec;
 
 static regex_t_size: uint = 64;
 
@@ -14,23 +13,55 @@ extern {
                nmatch: size_t, pmatch: *mut u8, eflags: c_int) -> c_int;
 }
 
-#[fixed_stack_segment]
-fn main() {
-    let mut reg: [u8, .. regex_t_size];
-    unsafe {
-        do "asd".with_c_str() |c_buffer| {
-            regcomp(vec::raw::to_mut_ptr(&mut reg),
-                    c_buffer, REG_EXTENDED as c_int);
+pub struct Regex {
+    priv preg: *u8
+}
+
+impl Regex {
+
+    pub fn new(regex: &str) -> Regex {
+        #[fixed_stack_segment];
+        #[inline(never)];
+        unsafe {
+            let preg = malloc(regex_t_size as u64) as *mut u8;
+            assert!(! ptr::is_null(preg));
+            do regex.with_c_str() |c_regex| {
+                regcomp(preg, c_regex, REG_EXTENDED as c_int);
+            }
+            Regex{preg: preg as *u8}
         }
     }
-    let mut found = false;
-    unsafe {
-        do "asdqwe".with_c_str() |c_buffer| {
-            if regexec(ptr::to_unsafe_ptr(reg as &[u8]),
-                       c_buffer, 0, ptr::mut_null(), 0 as c_int) == 0 {
-                found = true;
+
+    pub fn find(&self, string: &str) -> bool {
+        #[fixed_stack_segment];
+        #[inline(never)];
+        unsafe {
+            do string.with_c_str() |c_string| {
+                0 == regexec(self.preg, c_string,
+                             0, ptr::mut_null(), 0 as c_int)
             }
         }
     }
-    println!("found = {}", found);
+
+}
+
+impl Drop for Regex {
+    fn drop(&mut self) {
+        #[fixed_stack_segment];
+        #[inline(never)];
+        unsafe {
+            free(self.preg as *c_void);
+        }
+    }
+}
+
+fn main() {
+    let reg = Regex::new(std::os::args()[1]);
+    let stdin = std::io::stdin();
+    while ! stdin.eof() {
+        let line = stdin.read_line();
+        if reg.find(line) {
+            println(line);
+        }
+    }
 }
