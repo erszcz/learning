ed120e72a1d77ecd616e63acb282b02a30cfb5cd
Radek Szymczyszyn <lavrin@gmail.com>
2014-06-15 15:45:09 +0200

    Use Fibonacci sequence lazy generator

--- rust-euler/even_fibonacci.rs
+++ rust-euler/even_fibonacci.rs
@@ -1,9 +1,15 @@
+use std::iter::Unfold;
+
 fn main() {
-  let mut fib = fib_new();
-  for _ in range(1, 10) {
-    print!("{} ", fib_next(&mut fib))
+  for i in fibonacci().take(10) {
+    print!("{} ", i)
   }
   println!("")
+}
+
+fn fibonacci() -> Unfold<uint, FibState> {
+  let st = FibState{curr: 1, next: 2};
+  Unfold::new(st, fib_next)
 }
 
 struct FibState {
@@ -11,20 +17,9 @@ struct FibState {
   next: uint
 }
 
-fn fib_new() -> FibState {
-  FibState{
-    curr: 1u,
-    next: 1u
-  }
-}
-
-fn fib_next(st: &mut FibState) -> uint {
+fn fib_next(st: &mut FibState) -> Option<uint> {
   let prev = st.curr;
   st.curr = st.next;
   st.next = prev + st.next;
-  prev
+  Some (prev)
 }
-
-//fn fibonacci(n: uint) -> std::iter::Unfold<uint> {
-//  std::iter::Unfold::new(0u, |&mut s| s+1)
-//}
