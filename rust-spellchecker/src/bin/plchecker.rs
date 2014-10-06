4ba3861a4727a37f2688705ec69ae627f2670107
Radek Szymczyszyn <lavrin@gmail.com>
2014-10-07 00:38:52 +0200

    plchecker: find best corrections

--- rust-spellchecker/src/bin/plchecker.rs
+++ rust-spellchecker/src/bin/plchecker.rs
@@ -2,6 +2,7 @@ extern crate nlp;
 
 use std::io::BufferedReader;
 use std::io::File;
+use std::slice::Items;
 
 use nlp::distance::levenshtein as distance;
 
@@ -26,6 +27,10 @@ impl Dict {
         self.items.iter().any(|other_word| *word == *other_word)
     }
 
+    fn iter<'a>(&'a self) -> Items<'a, String> {
+        self.items.iter()
+    }
+
 }
 
 struct Word<'a> {
@@ -40,7 +45,7 @@ struct Word<'a> {
 
 impl<'a> Word<'a> {
 
-    fn new<'a>(s: &str, dict: &'a Dict) -> Word<'a> {
+    fn new(s: &str, dict: &'a Dict) -> Word<'a> {
         Word{word: s.to_string(),
              dict: dict,
              distances: None}
@@ -50,18 +55,49 @@ impl<'a> Word<'a> {
         self.dict.contains(&self.word)
     }
 
-    fn best_corrections(&self) -> Vec<String> {
-        vec!()
+    fn best_corrections(self, n: uint) -> Vec<Correction> {
+        let unwrapped_distances = self.distances.unwrap();
+        let mut word_distances : Vec<(&String, &uint)> =
+            self.dict.iter().zip(unwrapped_distances.iter()).collect();
+        word_distances.sort_by(|&(_,a), &(_,b)| a.cmp(b));
+        word_distances
+            .iter()
+            .map(|&(word, score)|
+                 Correction { word: word.to_string(),
+                              score: *score }).take(n).collect()
+    }
+
+    fn calculate_distances(&mut self) {
+        if self.distances.is_some()
+            { return }
+        let distances =
+            self.dict.iter().map(|other_word| {
+                distance(self.word.as_slice(), other_word.as_slice())
+            }).collect();
+        self.distances = Some(distances)
     }
 
 }
 
+struct Correction {
+    word: String,
+    score: uint
+}
+
 fn main() {
     let dict = Dict::new(DATA_PATH);
-    let to_check = Word::new( std::os::args()[1].as_slice(), &dict );
-    if to_check.is_valid() {
-        println!("ok!")
-    } else {
-        println!("not a valid word")
+    let mut to_check = Word::new( std::os::args()[1].as_slice(), &dict );
+    let ncorrections = 5u;
+    if to_check.is_valid()
+        { println!("ok!") }
+    else {
+        println!("not a valid word");
+        to_check.calculate_distances();
+        println!("did you mean?");
+        for correction in to_check.best_corrections(ncorrections).iter() {
+            println!("- {:s} ({:u})",
+                     correction.word.as_slice(),
+                     correction.score);
+        }
     }
 }
