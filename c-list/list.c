4ab9af82eff2e3b655d344adac1291be130c8d24
Radek Szymczyszyn <lavrin@gmail.com>
2015-01-12 21:16:43 +0100

    Detect cycle in a linked list

--- c-list/list.c
+++ c-list/list.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -40,6 +41,18 @@ delete(struct item** root) {
     }
 }
 
+bool
+is_cycle(struct item* list1) {
+    struct item* list2 = list1;
+    while (list1 != NULL && list2 != NULL && list2->next != NULL) {
+        list1 = list1->next;
+        list2 = list2->next->next;
+        if (list1 == list2)
+            return true;
+    }
+    return false;
+}
+
 void print_element(struct item*, char const*);
 
 void
@@ -68,6 +81,8 @@ int main(int argc, const char *argv[])
     insert(&list, 5);
     print(list);
 
+    struct item* last = list;
+
     insert(&list, 3);
     print(list);
 
@@ -75,6 +90,22 @@ int main(int argc, const char *argv[])
     print(list);
 
     print_element(find(list, 3), "\n");
+
+    assert(!is_cycle(list));
+    printf("list *is not* a cycle: ");
+    print(list);
+
+    last->next = list;
+    assert(is_cycle(list));
+    printf("list *is* a cycle: ");
+
+    int i;
+    struct item* p = list;
+    for (i = 0; i < 7; i++) {
+        print_element(p, " -> ");
+        p = p->next;
+    }
+    print_element(p, " ...\n");
 
     delete(&list);
     assert(list == NULL);
