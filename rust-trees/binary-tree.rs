d159f906436c9aec688121f20163d917b7d34954
Radek Szymczyszyn <lavrin@gmail.com>
2015-01-17 13:08:07 +0100

    Perform binary tree pre-/inorder traversals without recursion

--- rust-trees/binary-tree.rs
+++ rust-trees/binary-tree.rs
@@ -1,5 +1,7 @@
 #![feature(box_syntax)]
+use std::fmt::String;
 
+#[derive(Clone)]
 struct Tree<T> {
     l    : Option<Box<Tree<T>>>,
     r    : Option<Box<Tree<T>>>,
@@ -54,26 +56,67 @@ fn height_r<'b, T: Ord>(tree: &'b Option<Box<Tree<T>>>, acc: usize)
     }
 }
 
+// Perform a preorder traversal not using recursion.
+fn preorder_traversal<T: Ord+String+Clone, F: FnMut(T)>
+                     (tree: Option<Box<Tree<T>>>, mut f: F) {
+    if tree.is_none()
+        { return }
+    let mut stack = vec!();
+    stack.push((*tree.unwrap()));
+    while !stack.is_empty() {
+        let this = stack.pop().expect("popped from an empty stack");
+        f(this.data);
+        if this.r.is_some() { stack.push(*this.r.unwrap()) }
+        if this.l.is_some() { stack.push(*this.l.unwrap()) }
+    }
+}
+
+// Perform an inorder traversal not using recursion.
+fn inorder_traversal <T: Ord+String+Clone, F: FnMut(T)>
+                     (tree: Option<Box<Tree<T>>>, mut f: F) {
+    enum Visit { Descent, Ascent }
+    if tree.is_none()
+        { return }
+    let mut stack = vec!();
+    stack.push( (Visit::Descent, *tree.unwrap()) );
+    while !stack.is_empty() {
+        let (visit, this) = stack.pop().expect("popped from an empty stack");
+        match visit {
+            Visit::Descent => {
+                stack.push( (Visit::Ascent, this.clone()) );
+                if this.l.is_some()
+                    { stack.push( (Visit::Descent, *this.l.unwrap()) ) }
+            },
+            Visit::Ascent => {
+                f(this.data);
+                if this.r.is_some() {
+                    stack.push( (Visit::Descent, *this.r.unwrap()) );
+                }
+            }
+        }
+    }
+}
+
 #[test]
 fn search_in_one_element_tree() {
     let t = Tree { l: None, r: None, data: 123 };
-    let res: i64 = *search(123, &Some(box t)).unwrap();
+    let res: i32 = *search(123, &Some(box t)).unwrap();
     assert_eq!(123, res);
 }
 
 #[test]
 fn search_left_in_two_level_deep_tree() {
-    search_in_two_level_deep_tree(23i64);
+    search_in_two_level_deep_tree(23);
 }
 
 #[test]
 fn search_right_in_two_level_deep_tree() {
-    search_in_two_level_deep_tree(456i64);
+    search_in_two_level_deep_tree(456);
 }
 
 #[test]
 fn search_root_in_two_level_deep_tree() {
-    search_in_two_level_deep_tree(123i64);
+    search_in_two_level_deep_tree(123);
 }
 
 #[test]
@@ -83,10 +126,10 @@ fn search_nonexistent_in_tree() {
 }
 
 #[cfg(test)]
-fn search_in_two_level_deep_tree(expected: i64) {
-    let l = Tree { l: None, r: None, data: 23i64 };
-    let r = Tree { l: None, r: None, data: 456i64 };
-    let root = Tree { l: Some (box l), r: Some (box r), data: 123i64 };
+fn search_in_two_level_deep_tree(expected: i32) {
+    let l = Tree { l: None, r: None, data: 23 };
+    let r = Tree { l: None, r: None, data: 456 };
+    let root = Tree { l: Some (box l), r: Some (box r), data: 123 };
     let res = *search(expected, &Some(box root)).unwrap();
     assert_eq!(expected, res);
 }
@@ -99,7 +142,31 @@ fn height_of_one_element_tree() {
 
 #[test]
 fn height_of_two_element_tree() {
-    let r = Tree { l: None, r: None, data: 456i64 };
-    let root = Tree { l: None, r: Some (box r), data: 123i64 };
+    let r = Tree { l: None, r: None, data: 456 };
+    let root = Tree { l: None, r: Some (box r), data: 123 };
     assert_eq!(1, height(&Some(box root)));
+}
+
+#[test]
+fn preorder_traversal_test() {
+    let l = Tree { l: None, r: None, data: 23 };
+    let r = Tree { l: None, r: None, data: 456 };
+    let root = Tree { l: Some (box l), r: Some (box r), data: 123 };
+    let mut v: Vec<i32> = vec!();
+    preorder_traversal(Some (box root),
+                       |&mut:node_data|{ print!("{} ", node_data);
+                                         v.push(node_data) });
+    assert_eq!(v, vec![123, 23, 456]);
+}
+
+#[test]
+fn inorder_traversal_test() {
+    let l = Tree { l: None, r: None, data: 23 };
+    let r = Tree { l: None, r: None, data: 456 };
+    let root = Tree { l: Some (box l), r: Some (box r), data: 123 };
+    let mut v: Vec<i32> = vec!();
+    inorder_traversal(Some (box root),
+                      |&mut:node_data|{ print!("{} ", node_data);
+                                        v.push(node_data) });
+    assert_eq!(v, vec![23, 123, 456]);
 }
