d2fe0d5274c9efc12cb97ad9c7f72a1bcf9eafe5
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2016-04-07 13:50:08 +0200

    Erlang: upon which actions is gen_server:terminate really called?

---
+++ erlang-gen-server-terminate/src/driver.erl
@@ -0,0 +1,73 @@
+-module(driver).
+-compile([export_all]).
+
+%%
+%% Constants
+%%
+
+actions() ->
+    [#{op => stop, trap_exit => true},
+     #{op => crash, trap_exit => true},
+     #{op => external_normal_exit, trap_exit => true},
+     #{op => external_abnormal_exit, trap_exit => true},
+     #{op => linked_process_exits, trap_exit => true},
+     #{op => linked_process_crashes, trap_exit => true},
+
+     #{op => stop, trap_exit => false},
+     #{op => crash, trap_exit => false},
+     #{op => external_normal_exit, trap_exit => false},
+     #{op => external_abnormal_exit, trap_exit => false},
+     #{op => linked_process_exits, trap_exit => false},
+     #{op => linked_process_crashes, trap_exit => false}].
+
+%%
+%% API
+%%
+
+test() ->
+    Rs = [ do(A) || A <- actions() ],
+    [ print("~s~n", [format_result(R)]) || R <- Rs ].
+
+do(#{op := Op, trap_exit := Trap}) ->
+    try
+        CRef = make_ref(),
+        {ok, Pid} = server:start(self(), CRef),
+        MRef = erlang:monitor(process, Pid),
+        if
+            Trap -> server:trap_exit(Pid, Trap);
+            not Trap -> ok
+        end,
+        catch server:Op(Pid),
+        TStatus = receive
+                      {terminated, CRef} -> called
+                  after timer:seconds(1) -> not_called
+                  end,
+        MStatus = receive
+                      {'DOWN', MRef, _, _, _} ->
+                          ok
+                  after timer:seconds(10) ->
+                          timeout
+                  end,
+        #{op => Op,
+          trap_exit => Trap,
+          terminate_status => TStatus,
+          monitor_status => MStatus,
+          aliveness => erlang:is_process_alive(Pid)}
+    catch
+        _:R -> print("driver error: ~p~n", [R]),
+               driver_error
+    end.
+
+%%
+%% Internal
+%%
+
+print(Fmt, Args) ->
+    io:format(Fmt, Args).
+
+format_result(driver_error) -> "driver_error";
+format_result(#{op := Op, trap_exit := Trap,
+                terminate_status := TStatus,
+                aliveness := Alive}) ->
+    io_lib:format("op: ~p\ttrap: ~p\talive: ~p\tterminate: ~s\t",
+                  [Op, Trap, Alive, TStatus]).
---
+++ erlang-gen-server-terminate/src/server.erl
@@ -0,0 +1,122 @@
+-module(server).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/2,
+         start/2,
+         trap_exit/2,
+         stop/1,
+         external_normal_exit/1,
+         external_abnormal_exit/1,
+         crash/1,
+         linked_process_exits/1,
+         linked_process_crashes/1]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+%% Debug
+-export([trace/0, trace/1]).
+
+%%
+%% API
+%%
+
+start_link(Parent, Ref) ->
+    gen_server:start_link(?MODULE, [Parent, Ref], []).
+
+start(Parent, Ref) ->
+    gen_server:start(?MODULE, [Parent, Ref], []).
+
+trap_exit(Pid, Enabled) ->
+    gen_server:call(Pid, {trap_exit, Enabled}).
+
+stop(Pid) ->
+    gen_server:call(Pid, stop).
+
+crash(Pid) ->
+    gen_server:call(Pid, crash).
+
+external_normal_exit(Pid) ->
+    exit(Pid, normal).
+
+external_abnormal_exit(Pid) ->
+    exit(Pid, abnormal).
+
+linked_process_exits(Pid) ->
+    gen_server:cast(Pid, linked_process_exits).
+
+linked_process_crashes(Pid) ->
+    gen_server:cast(Pid, linked_process_crashes).
+
+%%
+%% gen_server callbacks
+%%
+
+init([Parent, Ref]) ->
+    {ok, #{parent => Parent, ref => Ref}}.
+
+handle_call({trap_exit, Enabled}, _From, State) ->
+    process_flag(trap_exit, Enabled),
+    {reply, ok, State};
+handle_call(stop, _From, State) ->
+    {stop, normal, ok, State};
+handle_call(crash, _From, State) ->
+    _ = 1 / 0,
+    {reply, ok, State};
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+handle_cast(linked_process_exits, State) ->
+    Pid = spawn_link(fun () -> ok end),
+    MRef = erlang:monitor(process, Pid),
+    receive {'DOWN', MRef, _, _, _} -> ok
+    after 1000 -> ok
+    end,
+    {noreply, State};
+handle_cast(linked_process_crashes, State) ->
+    Pid = spawn_link(fun () -> _ = 1 / 0 end),
+    MRef = erlang:monitor(process, Pid),
+    receive {'DOWN', MRef, _, _, _} -> ok
+    after 500 -> ok
+    end,
+    {noreply, State};
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(Reason, State) ->
+    maps:get(parent, State) ! {terminated, maps:get(ref, State)}, 
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%
+%% Debug
+%%
+
+trace() ->
+    trace(x).
+
+trace(Flags) ->
+    dbg:tpl(?MODULE, Flags).
+
+%%
+%% Internal functions
+%%
+
+print(Text) ->
+    print(Text, []).
+
+print(Fmt, Args) ->
+    io:format(Fmt, Args).
