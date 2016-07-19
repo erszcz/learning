908e53f4a1a3b3cb89b9c0c37e8cd8e2fea9cadc
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2016-07-19 17:44:23 +0200

    electron: Another approach

--- particle-electron/publish.ino
+++ particle-electron/publish.ino
@@ -7,16 +7,6 @@ void setup() {
     //tell the device we want to write to this pin
     pinMode(PIN, OUTPUT);
     i = 0;
-
-    Spark.publish("spark-setup-no-arg");
-    Spark.publish("spark-setup", "fixed-arg");
-    Spark.publish("spark-setup", "with-ttl", 21600);
-    Spark.publish("spark-setup", "with-ttl-explicitly-public", 21600, PUBLIC);
-
-    Particle.publish("particle-setup-no-arg");
-    Particle.publish("particle-setup", "fixed-arg");
-    Particle.publish("particle-setup", "with-ttl", 21600);
-    Particle.publish("particle-setup", "with-ttl-explicitly-public", 21600, PUBLIC);
 }
 
 void loop() {
@@ -28,19 +18,13 @@ void loop() {
 
     // bump the publish counter
     if (i % 5 == 0) {
+        // Doesn't work. No idea why.
+        //Spark.publish("spark-loop-no-arg");
+        // Works fine. No idea why either.
         Spark.publish("its-alive", String(i));
-        delay(20);
-        Spark.publish("spark-loop-no-arg");
-        delay(20);
-        Spark.publish("spark-loop", "fixed-arg");
-        delay(20);
-        Spark.publish("spark-loop", "with-ttl", 21600);
-        delay(20);
-        Spark.publish("spark-loop", "with-ttl-explicitly-public", 21600, PUBLIC);
-        delay(20);
     }
     i += 1;
 
     // wait a second
-    delay(900);
+    delay(1000);
 }
