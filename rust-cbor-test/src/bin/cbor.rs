8db3b6b645683ba9340a641d21d2972855ea79fe
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-06-30 17:00:33 +0200

    Read stdin after interpreting command line args

--- rust-cbor-test/src/bin/cbor.rs
+++ rust-cbor-test/src/bin/cbor.rs
@@ -6,13 +6,14 @@ use serde_cbor::ser::{ to_writer };
 use serde_json::{ to_string_pretty };
 
 fn main() {
-    let value: Value = from_reader(std::io::stdin()).unwrap();
     let args = std::env::args();
     match args.last().expect("cbor: --to-json or --to-cbor?").as_str() {
         "--to-json" => {
+            let value: Value = from_reader(std::io::stdin()).unwrap();
             println!("{}", to_string_pretty(&value).unwrap());
         }
         "--to-cbor" => {
+            let value: Value = from_reader(std::io::stdin()).unwrap();
             let _ = to_writer(&mut std::io::stdout(), &value).unwrap();
         }
         _ => panic!("cbor: unknown option")
