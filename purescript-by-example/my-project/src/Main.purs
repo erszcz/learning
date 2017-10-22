664b011fae2c58e1bd7afe18fd1d166a60846667
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-10-21 23:54:10 -0400

    purescript: End of chapter 2

--- purescript-by-example/my-project/src/Main.purs
+++ purescript-by-example/my-project/src/Main.purs
@@ -2,8 +2,11 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, log, logShow)
+import Math (sqrt)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "Hello sailor!"
+  --log "Hello sailor!"
+  logShow $ diagonal 3.0 4.0
+
+diagonal w h = sqrt (w * w + h * h)
