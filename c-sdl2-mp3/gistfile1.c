29e565a7b0425e24dd2ee41757be489ca996739b
Radek Szymczyszyn <lavrin@gmail.com>
2019-01-19 21:40:47 +0100

    c/sdl2: add SDL2 hello worlds

---
+++ c-sdl2-mp3/gistfile1.c
@@ -0,0 +1,35 @@
+// stolen from https://gist.github.com/cdave1/10563386
+// build with g++ gistfile1.c -o gistfile1 -lSDL2 -lSDL2_mixer
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_mixer.h"
+
+static const char *MY_COOL_MP3 = "cool_tunes.mp3";
+
+int main(int argc, char **argv) {
+    int result = 0;
+    int flags = MIX_INIT_MP3;
+
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        printf("Failed to init SDL\n");
+        exit(1);
+    }
+
+    if (flags != (result = Mix_Init(flags))) {
+        printf("Could not initialize mixer (result: %d).\n", result);
+        printf("Mix_Init: %s\n", Mix_GetError());
+        exit(1);
+    }
+
+    Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 640);
+    Mix_Music *music = Mix_LoadMUS(MY_COOL_MP3);
+    Mix_PlayMusic(music, 1);
+
+    while (!SDL_QuitRequested()) {
+        SDL_Delay(250);
+    }
+
+    Mix_FreeMusic(music);
+    SDL_Quit();
+    return 0;
+}
