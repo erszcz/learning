f5f20e176a50e158945c7cd38504963ec20aa0bd
Radek Szymczyszyn <lavrin@gmail.com>
2019-06-26 20:04:58 +0200

    rust/wasm: First exercise - make greet() take a name

--- rust-wasm-tutorial/wasm-game-of-life/src/lib.rs
+++ rust-wasm-tutorial/wasm-game-of-life/src/lib.rs
@@ -14,6 +14,6 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}", name));
 }
---
+++ rust-wasm-tutorial/wasm-game-of-life/www/index.js
@@ -0,0 +1,3 @@
+import * as wasm from "wasm-game-of-life";
+
+wasm.greet("Radek");
