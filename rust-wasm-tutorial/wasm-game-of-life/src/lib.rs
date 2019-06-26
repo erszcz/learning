f8ef4964ad8b40b884830ce5e4e46c4f2145dc5f
Radek Szymczyszyn <lavrin@gmail.com>
2019-06-26 20:33:53 +0200

    rust/wasm: Game of Life rendered to a <canvas/>

--- rust-wasm-tutorial/wasm-game-of-life/src/lib.rs
+++ rust-wasm-tutorial/wasm-game-of-life/src/lib.rs
@@ -84,6 +84,18 @@ impl Universe {
     pub fn render(&self) -> String {
         self.to_string()
     }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
 }
 
 impl Universe {
