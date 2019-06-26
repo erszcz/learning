e72a73714437a2156ab0e69751cbfa93d1e926ec
Radek Szymczyszyn <lavrin@gmail.com>
2019-06-26 21:38:48 +0200

    rust/wasm: test game of life rules in browser context

--- rust-wasm-tutorial/wasm-game-of-life/src/lib.rs
+++ rust-wasm-tutorial/wasm-game-of-life/src/lib.rs
@@ -119,6 +119,36 @@ impl Universe {
         }
         count
     }
+
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+    }
+
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
 }
 
 impl fmt::Display for Universe {
