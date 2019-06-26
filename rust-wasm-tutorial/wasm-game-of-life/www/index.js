829364e66c02ee1f299f47fd6567711a4ed5d95a
Radek Szymczyszyn <lavrin@gmail.com>
2019-06-26 20:23:59 +0200

    rust/wasm: Game of Life rendered as a <pre/>formatted string

--- rust-wasm-tutorial/wasm-game-of-life/www/index.js
+++ rust-wasm-tutorial/wasm-game-of-life/www/index.js
@@ -1,3 +1,11 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("Radek");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+requestAnimationFrame(renderLoop);
