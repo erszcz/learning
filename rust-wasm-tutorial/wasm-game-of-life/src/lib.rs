a246cebd72c7a0cf92b7f4197ea88d9ba83086ab
Radek Szymczyszyn <lavrin@gmail.com>
2019-06-27 08:42:16 +0200

    rust/wasm: increase Universe size to see differences in the profile

--- rust-wasm-tutorial/wasm-game-of-life/src/lib.rs
+++ rust-wasm-tutorial/wasm-game-of-life/src/lib.rs
@@ -76,8 +76,8 @@ impl Universe {
     pub fn new() -> Universe {
         utils::set_panic_hook();
 
-        let width = 64;
-        let height = 64;
+        let width = 128;
+        let height = 128;
 
         let cells = (0..width * height)
             .map(|i| {
