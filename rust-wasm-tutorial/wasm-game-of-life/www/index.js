f36c953672fb78b3e0be7b5448e40116c829155e
Radek Szymczyszyn <lavrin@gmail.com>
2019-06-27 19:34:20 +0200

    rust/wasm: set ctx.fillStyle only twice per frame

--- rust-wasm-tutorial/wasm-game-of-life/www/index.js
+++ rust-wasm-tutorial/wasm-game-of-life/www/index.js
@@ -89,14 +89,30 @@ const drawCells = () => {
 
   ctx.beginPath();
 
+
+  ctx.fillStyle = DEAD_COLOR;
   for (let row = 0; row < height; row++) {
     for (let col = 0; col < width; col++) {
       const idx = getIndex(row, col);
+      if (cells[idx] !== Cell.Dead) {
+        continue;
+      };
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
 
-      ctx.fillStyle = cells[idx] === Cell.Dead
-        ? DEAD_COLOR
-        : ALIVE_COLOR;
-
+  ctx.fillStyle = ALIVE_COLOR;
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+      if (cells[idx] !== Cell.Alive) {
+        continue;
+      };
       ctx.fillRect(
         col * (CELL_SIZE + 1) + 1,
         row * (CELL_SIZE + 1) + 1,
