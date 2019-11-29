e3e1712dc7d1151eda6ea167084a3048c4bfd8ca
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2019-11-29 11:44:37 +0100

    c-jids: copy data to jid_t for modification

--- c-jids/jid.cc
+++ c-jids/jid.cc
@@ -1,7 +1,9 @@
-#include <cstdlib>
 #include <cassert>
-#include <cstring>
+#include <cstdbool>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include "slimtests.h"
 
 //static ERL_NIF_TERM
 //from_binary_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
@@ -62,14 +64,24 @@
 //            resource);
 //}
 
+#define JID_T_LEN 256
+typedef struct {
+    char        data[JID_T_LEN];
+    const char* user;
+    const char* server;
+    const char* res;
+} jid_t;
+
 int mk_error() {
     abort();
     return 1;
 }
 
-int v1()
+int binary_to_jid_v1(const char* data_in, jid_t* jid)
 {
-    char* data = "alice@localhost/res1";
+    bzero(jid->data, JID_T_LEN);
+    memcpy(jid->data, data_in, strnlen(data_in, JID_T_LEN-1));
+    char* data = jid->data;
     const unsigned size = strlen(data);
     unsigned commercial_at = -1;
     unsigned slash = size;
@@ -93,29 +105,63 @@ end_loop:
     if (commercial_at == 0 || slash == 0) {
         return mk_error();
     }
-    char* user = data;
-    char* server = data+commercial_at+1;
-    char* resource = data+slash+1;
-    printf("%s\n", user);
-    printf("%s\n", server);
-    printf("%s\n", resource);
+    data[commercial_at] = 0;
+    data[slash] = 0;
+    jid->user = data;
+    jid->server = data+commercial_at+1;
+    jid->res = data+slash+1;
     return 0;
 }
 
-int v2()
+int binary_to_jid_v2(const char* data_in, jid_t* jid)
 {
-    char* data = "alice@localhost/res1";
-    char* user = data;
-    char* server = strpbrk(data, "@/");
-    //assert(server[0] == '@');
-    char* resource = strpbrk(server+1, "@/");
-    //assert(resource[0] == '/');
-    printf("%s\n", user);
-    printf("%s\n", server);
-    printf("%s\n", resource);
+    bzero(jid->data, JID_T_LEN);
+    memcpy(jid->data, data_in, strnlen(data_in, JID_T_LEN-1));
+    char* data = jid->data;
+    jid->user = data;
+    jid->server = strpbrk(data, "@/");
+    data[jid->server - data] = 0;
+    jid->server += 1;
+    jid->res = strpbrk(jid->server+1, "@/");
+    data[jid->res - data] = 0;
+    jid->res += 1;
     return 0;
 }
+
+typedef int (*binary_to_jid_t)(const char*, jid_t*);
+
+binary_to_jid_t binary_to_jid;
+
+const char* jid1 = "alice@example.com/res1";
+const char* jid2 = "example.com/res1";
+const char* jid3 = "example.com";
+const char* jid4 = "example.com/heres-the-@-sign";
+
+bool parses_user_server_resource() {
+    jid_t jid;
+    binary_to_jid(jid1, &jid);
+    printf(".user   = %s\n", jid.user);
+    printf(".server = %s\n", jid.server);
+    printf(".res    = %s\n", jid.res);
+    assert(0 == strcmp("alice", jid.user));
+    assert(0 == strcmp("example.com", jid.server));
+    assert(0 == strcmp("res1", jid.res));
+    return true;
+}
+
+test_spec tests[] = {
+    TEST_SPEC(parses_user_server_resource)
+};
+
+#define size(arr) (sizeof(arr) / sizeof(arr[0]))
 
 int main() {
-    v2();
+
+    fprintf(stderr, "Testing custom loop\n");
+    binary_to_jid = binary_to_jid_v1;
+    run_tests(tests, size(tests));
+
+    fprintf(stderr, "Testing strpbrk()\n");
+    binary_to_jid = binary_to_jid_v2;
+    run_tests(tests, size(tests));
 }
