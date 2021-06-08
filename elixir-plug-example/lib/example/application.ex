2bea4dc649e887c23046ade9d179244b7632e5af
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-06-08 15:19:26 +0200

    Define a simple working Plug app

--- elixir-plug-example/lib/example/application.ex
+++ elixir-plug-example/lib/example/application.ex
@@ -4,17 +4,20 @@ defmodule Example.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
     children = [
       # Starts a worker by calling: Example.Worker.start_link(arg)
       # {Example.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: Example.HelloWorldPlug, options: [port: 18080]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Example.Supervisor]
+    Logger.info("Starting application...")
     Supervisor.start_link(children, opts)
   end
 end
