51d61d3766ec88c547c4eb8ce04488e26396963a
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-08-31 16:10:56 +0200

    Elixir: some other ways of accessing the config

--- elixir-config-runtime/config/runtime.exs
+++ elixir-config-runtime/config/runtime.exs
@@ -4,3 +4,6 @@ config :iex, default_prompt: ">>>"
 
 config :config_runtime_test,
   opt1: "runtime value 1"
+
+config :config_runtime_test, ConfigRuntime,
+  opt1: "ConfigRuntime runtime value 1"
--- elixir-config-runtime/lib/config_runtime.ex
+++ elixir-config-runtime/lib/config_runtime.ex
@@ -5,8 +5,22 @@ defmodule ConfigRuntime do
 
   @opt1 Application.compile_env!(:config_runtime_test, :opt1)
 
+  defmodule Config do
+    def opt1, do: Application.get_env(:config_runtime_test, :opt1, "default value 1")
+  end
 
-  def hello do
+  def hello1 do
     "hello #{@opt1}"
+  end
+
+  def hello2 do
+    "hello #{config()[:opt1]}"
+  end
+
+  defp config do
+    defaults = %{opt1: "default value 1"}
+    config = Application.get_env(:config_runtime_test, __MODULE__, [])
+             |> Enum.into %{}
+    Map.merge(defaults, config)
   end
 end
