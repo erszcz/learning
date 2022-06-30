f544d95f3e94dece90142d4a7142d83e6be3a7e8
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-06-30 11:31:35 +0200

    Override the router to expose GraphiQL

--- elixir-graphql/community/lib/community_web/router.ex
+++ elixir-graphql/community/lib/community_web/router.ex
@@ -5,36 +5,12 @@ defmodule CommunityWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", CommunityWeb do
+  scope "/" do
     pipe_through :api
-  end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      live_dashboard "/dashboard", metrics: CommunityWeb.Telemetry
-    end
-  end
-
-  # Enables the Swoosh mailbox preview in development.
-  #
-  # Note that preview only shows emails that were sent by the same
-  # node running the Phoenix server.
-  if Mix.env() == :dev do
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: CommunityWeb.Schema,
+      interface: :simple,
+      context: %{pubsub: CommunityWeb.Endpoint}
   end
 end
