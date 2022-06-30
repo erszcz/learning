4328978d989e1550498fa3652cd45e11222ec067
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-06-30 11:46:29 +0200

    Define a mutation and its resolver

--- elixir-graphql/community/lib/community/web/resolvers/news_resolver.ex
+++ elixir-graphql/community/lib/community/web/resolvers/news_resolver.ex
@@ -4,4 +4,15 @@ defmodule CommunityWeb.NewsResolver do
   def all_links(_root, _args, _info) do
     {:ok, News.list_links()}
   end
+
+  def create_link(_root, args, _info) do
+    # TODO: add detailed error message handling later
+    case News.create_link(args) do
+      {:ok, link} ->
+        {:ok, link}
+
+      _error ->
+        {:error, "could not create link"}
+    end
+  end
 end
--- elixir-graphql/community/lib/community_web/schema.ex
+++ elixir-graphql/community/lib/community_web/schema.ex
@@ -15,4 +15,14 @@ defmodule CommunityWeb.Schema do
       resolve(&NewsResolver.all_links/3)
     end
   end
+
+  mutation do
+    @desc "Create a new link"
+    field :create_link, :link do
+      arg(:url, non_null(:string))
+      arg(:description, non_null(:string))
+
+      resolve(&NewsResolver.create_link/3)
+    end
+  end
 end
