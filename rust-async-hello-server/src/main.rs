bd02cd116230b008445cb5b9a68391e16123bb20
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2023-09-12 21:45:25 +0200

    rust: concurrent single-threaded server with a slow request handler

--- rust-async-hello-server/src/main.rs
+++ rust-async-hello-server/src/main.rs
@@ -1,27 +1,32 @@
 use std::fs;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
 use std::time::Duration;
+
+use async_std::net::TcpListener;
+use async_std::net::TcpStream;
+use async_std::prelude::*;
 use async_std::task;
+use futures::stream::StreamExt;
 
 #[async_std::main]
 async fn main() {
     // Listen for incoming TCP connections on localhost port 7878
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
 
     // Block forever, handling each request that arrives at this IP address
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    listener
+        .incoming()
+        .for_each_concurrent(None, |tcpstream| async move {
+            let tcpstream = tcpstream.unwrap();
 
-        handle_connection(stream).await;
-    }
+            handle_connection(tcpstream).await;
+        })
+        .await;
 }
 
 async fn handle_connection(mut stream: TcpStream) {
     // Read the first 1024 bytes of data from the stream
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
@@ -41,6 +46,6 @@ async fn handle_connection(mut stream: TcpStream) {
     // Write response back to the stream,
     // and flush the stream to ensure the response is sent back to the client
     let response = format!("{status_line}{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
