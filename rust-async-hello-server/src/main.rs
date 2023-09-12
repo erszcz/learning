3b5f2bbdcde880bed7b6d0d5b5ca6dd971d4b2dd
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2023-09-12 21:34:08 +0200

    rust: save a non-concurrent, blocking version of the seemingly async server

--- rust-async-hello-server/src/main.rs
+++ rust-async-hello-server/src/main.rs
@@ -2,8 +2,11 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::time::Duration;
+use async_std::task;
 
-fn main() {
+#[async_std::main]
+async fn main() {
     // Listen for incoming TCP connections on localhost port 7878
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -11,20 +14,24 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        handle_connection(stream).await;
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
     // Read the first 1024 bytes of data from the stream
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     // Respond with greetings or a 404,
     // depending on the data in the request
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        task::sleep(Duration::from_secs(5)).await;
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
