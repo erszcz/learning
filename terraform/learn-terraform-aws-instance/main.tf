f56aabb1771b1f7c104c0893b39d4732a75ce535
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2024-02-20 14:40:06 +0100

    terraform: change the instance AMI

--- terraform/learn-terraform-aws-instance/main.tf
+++ terraform/learn-terraform-aws-instance/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
