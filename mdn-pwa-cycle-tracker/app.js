3d6443687ea241ea28cb58c0af4f7e3ae48870c2
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2024-05-09 15:10:20 +0200

    Save app.js to make it a functional webapp

---
+++ mdn-pwa-cycle-tracker/app.js
@@ -0,0 +1,73 @@
+const newPeriodFormEl = document.getElementsByTagName("form")[0];
+const startDateInputEl = document.getElementById("start-date");
+const endDateInputEl = document.getElementById("end-date");
+const pastPeriodContainer = document.getElementById("past-periods");
+
+// Add the storage key as an app-wide constant
+const STORAGE_KEY = "period-tracker";
+
+// Listen to form submissions.
+newPeriodFormEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const startDate = startDateInputEl.value;
+  const endDate = endDateInputEl.value;
+  if (checkDatesInvalid(startDate, endDate)) {
+    return;
+  }
+  storeNewPeriod(startDate, endDate);
+  renderPastPeriods();
+  newPeriodFormEl.reset();
+});
+
+function checkDatesInvalid(startDate, endDate) {
+  if (!startDate || !endDate || startDate > endDate) {
+    newPeriodFormEl.reset();
+    return true;
+  }
+  return false;
+}
+
+function storeNewPeriod(startDate, endDate) {
+  const periods = getAllStoredPeriods();
+  periods.push({ startDate, endDate });
+  periods.sort((a, b) => {
+    return new Date(b.startDate) - new Date(a.startDate);
+  });
+  window.localStorage.setItem(STORAGE_KEY, JSON.stringify(periods));
+}
+
+function getAllStoredPeriods() {
+  const data = window.localStorage.getItem(STORAGE_KEY);
+  const periods = data ? JSON.parse(data) : [];
+  console.dir(periods);
+  console.log(periods);
+  return periods;
+}
+
+function renderPastPeriods() {
+  const pastPeriodHeader = document.createElement("h2");
+  const pastPeriodList = document.createElement("ul");
+  const periods = getAllStoredPeriods();
+  if (periods.length === 0) {
+    return;
+  }
+  pastPeriodContainer.innerHTML = "";
+  pastPeriodHeader.textContent = "Past periods";
+  periods.forEach((period) => {
+    const periodEl = document.createElement("li");
+    periodEl.textContent = `From ${formatDate(
+      period.startDate,
+    )} to ${formatDate(period.endDate)}`;
+    pastPeriodList.appendChild(periodEl);
+  });
+
+  pastPeriodContainer.appendChild(pastPeriodHeader);
+  pastPeriodContainer.appendChild(pastPeriodList);
+}
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", { timeZone: "UTC" });
+}
+
+renderPastPeriods();
