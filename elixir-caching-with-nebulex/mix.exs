28a6e55c75d03ecb9ba57e41425dcca5a01d19b4
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2024-06-18 09:40:23 +0200

    elixir-nebulex: save project scaffolding

--- elixir-caching-with-nebulex/mix.exs
+++ elixir-caching-with-nebulex/mix.exs
@@ -14,7 +14,8 @@ defmodule CachingWithNebulex.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {CachingWithNebulex.Application, []}
     ]
   end
 
