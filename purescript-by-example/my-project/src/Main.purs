79c2c55570738e6a68950ed082b64ea86bcca419
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2025-01-22 09:36:25 +0100

    purescript-by-example: use Data.Number.sqrt from numbers package

--- purescript-by-example/my-project/src/Main.purs
+++ purescript-by-example/my-project/src/Main.purs
@@ -3,9 +3,13 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console (log, logShow)
+import Data.Number (sqrt)
 
 main :: Effect Unit
 main = do
   log "🍝"
+  logShow $ diagonal 3.0 4.0
 
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
